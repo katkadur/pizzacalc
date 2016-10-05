198c324abf52b9986a54c30385179b989a9128f3
katkadur <kd@mozog.com>
2016-10-04 21:24:43 -0500

    second commit of pizza code

--- main.cpp
+++ main.cpp
@@ -1,29 +1,55 @@
 #include <iostream>
 #include <string>
 
-
-int user() {
-    float numbpizza;
-    float numbpeople;
+int getuserinput() {
+    int numbpeople;
     float price;
+    int desiredslices;
     float slices;
-    std::cout<<"How many pizzas?"<< std::endl;
-    std::cin>>numbpizza;
-    //std::cout<<numbpizza <<std::endl;
-    std::cout<<"How many people?"<<std::endl;
+    int usernumbpizza;
+
+    std::cout<<"How many people are you feeding?"<<std::endl;
     std::cin>>numbpeople;
-    std::cout<<"How much does one pizza cost?"<<std::endl;
-    std::cin>>price;
+    while (numbpeople <= 0) {
+        std::cout<<"I'm sorry, you need some friends. Really, how many peoples you got?"<<std::endl;
+        std::cin>>numbpeople;
+    }
+    std::cout<<"Cool!! "<<numbpeople<< " people it is.\n"<<std::endl;
+
+    std::cout<<"How many slices does each person want? (p.s. YOU CAN ONLY EAT WHOLE PIECES OF PIZZA)"<<std::endl;
+    std::cin>>desiredslices;
+    if (desiredslices < 3) {
+        std::cout<<"Okay, "<<desiredslices<<" slices of pizza per person coming right up\n"<<std::endl;
+    }
+    else{
+        std::cout<<"Wow!! You guys must be really hungry if everyone wants "<<desiredslices<<" slices of pizza!\n"<<std::endl;
+    }
+
     std::cout<<"How many slices does each pizza have?"<<std::endl;
     std::cin>>slices;
+    while (slices <= 0) {
+        std::cout<<"I'm sorry, that sounds like a really invisible pizza. How many slices does it really have?"<<std::endl;
+        std::cin>>slices;
+    }
 
-    float priceforall = price * numbpizza;
+    std::cout<<"Cool beans! Each pizza has "<<slices<<" slices.\n"<<std::endl;
+
+    std::cout<<"How much does each pizza cost?"<<std::endl;
+    std::cin>>price;
+    std::cout<<"Okie dokie! Your answer is coming right up...\n"<<std::endl;
+
+    float totalslices = numbpeople * desiredslices;
+    float numbpizza = totalslices / slices;
+    usernumbpizza = numbpizza + 0.99;
+    float priceforall = usernumbpizza * price;
     float costperperson = priceforall / numbpeople;
-    float sliceperperson = (slices * numbpizza) / numbpeople;
-    std::cout<<"Each person gets "<< sliceperperson <<" slices."<<std::endl;
+    int leftovers = usernumbpizza % desiredslices;
+
+    std::cout<<"You will need to order "<<usernumbpizza<<" pizzas"<<std::endl;
+    std::cout<<"There will be "<<leftovers<<" pieces of pizza leftover"<<std::endl;
+    std::cout<<"The total price is $"<<priceforall<<std::endl;
     std::cout<<"The price per person is $"<<costperperson<<std::endl;
 
-    //std::cout<<numbpeople<<sy howtd::endl;
     return 0;
 }
 
@@ -31,7 +57,6 @@ int user() {
 
 
 int main(){
-    user();
-    std::cout << "hello idiot";
+    getuserinput();
     return 0;
 }
